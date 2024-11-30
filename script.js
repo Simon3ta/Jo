8dd0c54679ce2bdf45dbf6a088b7fb069894edd3
Simon3ta <164954539+Simon3ta@users.noreply.github.com>
2024-11-30 17:54:56 -0300

    actualizacion
    actualizacion

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function revealSurprise() {
     const surprise = document.getElementById('surprise');
+    const message = document.getElementById('message');
+    
     surprise.classList.remove('hidden');
+    surprise.classList.add('visible');
+    
+    message.classList.remove('hidden-message');
+    message.classList.add('visible');
 }
