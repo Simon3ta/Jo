fd848838777bdf984777627973d06af876c48f86
Simon3ta <164954539+Simon3ta@users.noreply.github.com>
2024-11-30 19:48:40 -0300

    actualizacion
    actualizacion

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function revealMessage() {
     const message = document.getElementById('message');
+    const song = document.getElementById('song');
+
+    // Mostrar el mensaje
     message.classList.remove('hidden-message');
     message.classList.add('visible');
+
+    // Reproducir la canción
+    song.play();
 }
+song.volume = 0.5; // 50% del volumen máximo
